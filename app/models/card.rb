e35e27b648a4568a56f4011ee4622d51f7a09763
fueelt <fueelt@gmail.com>
2017-02-13 00:19:31 +0300

    create validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,15 +1,20 @@
 class Card < ApplicationRecord
-	validates :review_date, presence: true
 	validates :original_text, presence: true
 	validates :translated_text, presence: true
-    validate :not_equal
+	validates :review_date, presence: true
 
-	VALID_REGEX = /\A[a-z]+\z/
-	validates :original_text, :translated_text, format: { with: VALID_REGEX }
+    before_create do
+       self.review_date = Date.today.next_day(3) 
+    end
+
+	validate :not_equal
+
+    validates_each :translated_text, :original_text do |record, attr, value|
+      record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+    end
 
     def not_equal
       errors.add(:base, 'translated text and original text cannot be equal.') if 
       translated_text == original_text
     end
-    
 end
