0bc5be1f8adf81d1c7208bccff0ab190e6174bc6
fueelt <fueelt@gmail.com>
2017-02-18 20:54:56 +0300

    with nil objects

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,18 +1,21 @@
 class Card < ApplicationRecord
-	validates :original_text, :translated_text, presence: true
-	validate :comparison 
-	
-	before_create do
-       self.review_date = 3.days.from_now 
-    end
+	validates :original_text, presence: true
+#	validate :comparison 
+
+
+  
+  before_create do
+    self.review_date = 3.days.from_now 
+    self.original_text.delete if self.translated_text.nil? 
+  end
 
 #	VALID_REGEX = /\A[a-z]+\z/i 
 #    validates :translated_text, :original_text, format: { with: VALID_REGEX }
     
-    def comparison
-      if original_text == translated_text
-        errors[:base] << 'Original text and translated_text text cannot be equal.' 
-      end
-    end
+#  def comparison
+#    if original_text == translated_text
+#      errors[:base] << 'Original text and translated_text text cannot be equal.' 
+#    end
+#  end
 
-end
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,44 @@ require 'open-uri'
 require 'nokogiri'
 
 Card.delete_all
-data = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
-cards = data.css(".rowB", ".rowA").each do |card|
-  original_text = card.css('.bigLetter').text
-  translated_text = card.css('td[3]').text
-  card = Card.create!(:original_text => original_text, :translated_text => translated_text, :review_date => Date.today )
-end
 
+DATA_DIR = "db/fcards"
+Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
+
+BASE_URL = "http://www.languagedaily.com"
+LIST_URL = "#{BASE_URL}/learn-german/vocabulary/common-german-words-11"
+
+HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
+
+page = Nokogiri::HTML(open(LIST_URL))
+rows = page.css('#jsn-mainbody > div.com-content > div > div.jsn-article-content > ul')
+
+rows.each do |row|
+  hrefs = row.css("li a").map{ |a|   #map захватывает данные url если он содержит languagedaily
+    a['href'] if a['href']=~ /^\/learn-german\//
+  }.compact.uniq  # compact удаление nil запросы, uniq - удаляет повторяющие данные
+
+  hrefs.each do |href|
+    remote_url = BASE_URL + href
+    local_fname = "#{DATA_DIR}/#{File.basename(href)}.html"
+    
+    unless File.exists?(local_fname)
+      begin
+        vocabulary_content = open(remote_url, HEADERS_HASH).read
+      rescue Exception=>e
+        sleep 5 # паузы между открытием страниц
+      else
+        File.open(local_fname, 'w'){|file| file.write(vocabulary_content)}
+        puts "\t...Success, saved to #{local_fname}"
+      ensure
+        sleep 1.0 + rand
+      end  
+    end 
+    list = Nokogiri::HTML(open("#{local_fname}"))
+    cards = list.css(".rowB", ".rowA").each do |card|
+      original_text = card.css('.bigLetter').text
+	  translated_text = card.css('td[3]').text
+	  card = Card.create!( original_text: original_text, translated_text: translated_text )
+	end
+  end 
+end 
