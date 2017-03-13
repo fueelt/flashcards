5919ee4aec059bc980e7665c21cbd905af60cae2
fueelt <fueelt@gmail.com>
2017-03-13 22:57:28 +0300

    problem - undefined method ..._text' for nil:NilClass

--- app/controllers/concerns/home_controller.rb
+++ app/controllers/concerns/home_controller.rb
@@ -1,6 +1,23 @@
 class HomeController < ApplicationController
+	before_action :show_card, except: [:index, :new, :create]
 
 	def index
 	end
+ 
+    def show
+      @card = Card.find(params[:id])
+    end
+  
+    def edit
+    end 
 
+private 
+  
+    def show_card
+      @card = Card.find(params[:id])
+    end 
+
+    def card_params
+      params.require(:card).permit(:translated_text, :original_text, :review_date)     
+    end
 end
